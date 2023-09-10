9ef6ae50ef10813130334cf65727c2df5e664a21
Tanuththiga <anuththiga25jt@gmail.com>
2023-09-11 01:19:52 +0800

    api integration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import axios from 'axios';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Header from './Components/Header';
+import { Container } from '@mui/system';
 
 function App() {
+  const [word, setWord] = useState('');
+  const [meaning, setMeaning] = useState([]);
+
+  const dictionaryApi = async() => {
+    try {
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+      );
+      setMeaning(data.data);
+    } catch (error) {
+      console.log(error)
+    }
+  };
+console.log(meaning)
+  useEffect(() => {
+    dictionaryApi();
+  }, [word])
+
   return (
-    <div className="App">
+    <div className="App" style={{height: "100vh", backgroundColor: "#282c34", color: "white"}}>
+      <Container maxWidth="md" style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+        <Header 
+          word={word}
+          setWord={setWord}
+        />
+      </Container>
       
     </div>
   );
